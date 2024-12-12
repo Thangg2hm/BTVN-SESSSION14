c6de3c717b744133952edc3a21c634934b81f3e7
Thangg2hm <thangnguyenlleq1207205@gmail.com>
2024-12-12 07:50:24 +0700

    EX2.cpp

--- EX2.cpp
+++ EX2.cpp
@@ -4,7 +4,7 @@ int main() {
     char chuoi[] = "Hello, World!";
     int i = 0;
 
-    // In ra t?ng ký t? trong chu?i, m?i ký t? cách nhau 1 d?u cách
+    
     while (chuoi[i] != '\0') {
         printf("%c ", chuoi[i]);
         i++;
